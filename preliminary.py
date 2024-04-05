73997cb4d98979fa293a745247e75a674581433a
Biniyam Aschalew <aschalbiniyam@gmail.com>
2024-04-06 01:25:18 +0900

    added device compatibility, added plotting function

--- preliminary.py
+++ preliminary.py
@@ -7,12 +7,15 @@ import numpy as np
 
 # setting a fixed seed
 seed_value = 19
+BATCH_SIZE = 4
 
 torch.manual_seed(seed_value)
 if torch.cuda.is_available():
     torch.cuda.manual_seed_all(seed_value)
 random.seed(seed_value)
 np.random.seed(seed_value)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 student_input_dict = { "size": [16, 32], "kernel_size": [3, 3], "stride": [1, 1], "padding": [0, 0], "dropout": [0, 0] }
@@ -22,36 +25,43 @@ student1 = ConvNetBuilder(1, 10, student_input_dict) # learns from hardlabeled m
 student2 = ConvNetBuilder(1, 10, student_input_dict) # learns from softlabeled mnist
 student3 = ConvNetBuilder(1, 10, student_input_dict) # no learning / random baseline
 
+student0.to(device)
+student1.to(device)
+student2.to(device)
+student3.to(device)
 
 teacher_input_dict = { "size": [16, 32, 64, 64], "kernel_size": [3, 3, 3, 3], "stride": [1, 1, 1, 1], "padding": [1, 1, 1, 1], "dropout": [0.5, 0.5, 0.5, 0.5] }
 teacher = ConvNetBuilder(1, 10, teacher_input_dict)
 
-trainloader, testloader = MNISTDataLoader(4).load_data()
-teacher = train_network(teacher, trainloader, 1, 0.001)
+teacher.to(device)
+
+trainloader, testloader = MNISTDataLoader(BATCH_SIZE).load_data()
+teacher = train_network(teacher, trainloader, 1, 0.001, device)
 
 print("finished training teacher")
-test_network(teacher, testloader)
+test_network(teacher, testloader, device)
 
-mnist_soft_labels_loader = get_soft_labels(teacher, trainloader)
+mnist_soft_labels_loader = get_soft_labels(teacher, trainloader, device)
 print("finished getting soft labels for MNIST")
 
 # mnist dataset has 60k images, for the noise we will generate 600k images
-noise_loader = generate_noise_dataloader(600000, 4)
-noise_soft_labels_loader = get_soft_labels(teacher, noise_loader)
+noise_loader = generate_noise_dataloader(600000, device, BATCH_SIZE)
+noise_soft_labels_loader = get_soft_labels(teacher, noise_loader, device)
+print("finished getting soft labels for noise")
 
-student0 = train_network(student0, noise_soft_labels_loader, 1, 0.001)
-student1 = train_network(student1, trainloader, 1, 0.001)
-student2 = train_network(student2, mnist_soft_labels_loader, 1, 0.001)
+student0 = train_network(student0, noise_soft_labels_loader, 1, 0.001, device)
+student1 = train_network(student1, trainloader, 1, 0.001, device)
+student2 = train_network(student2, mnist_soft_labels_loader, 1, 0.001, device)
 
 print("evaluating the four students")
 print("evaluating student 0 which is trained on soft labels from noise")
-test_network(student0, testloader)
+test_network(student0, testloader, device)
 
 print("evaluating student 1 which is trained on hard labels")
-test_network(student1, testloader)
+test_network(student1, testloader, device)
 
 print("evaluating student 2 which is trained on soft labels")
-test_network(student2, testloader)
+test_network(student2, testloader, device)
 
 print("evaluating student 3 which is not trained yet")
-test_network(student3, testloader)
+test_network(student3, testloader, device)
--- utils.py
+++ utils.py
@@ -2,9 +2,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
+import matplotlib.pyplot as plt
 
 
-def train_network(model, trainloader, epochs, lr):
+def train_network(model, trainloader, epochs, lr, device):
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
@@ -12,7 +13,10 @@ def train_network(model, trainloader, epochs, lr):
     for epoch in range(epochs):
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
+
             inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
@@ -27,13 +31,16 @@ def train_network(model, trainloader, epochs, lr):
     print("Finished Training")
     return model
 
-def test_network(model, testloader):
+def test_network(model, testloader, device):
 
     correct = 0
     total = 0
     with torch.no_grad():
         for data in testloader:
+
             images, labels = data
+            images, labels = images.to(device), labels.to(device)
+
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
@@ -43,7 +50,7 @@ def test_network(model, testloader):
     return correct / total
 
 
-def get_soft_labels(model, trainloader):
+def get_soft_labels(model, trainloader, device):
     model.eval()  # Set the model to evaluation mode
     soft_labels = []
     data_list = []
@@ -51,7 +58,10 @@ def get_soft_labels(model, trainloader):
 
     with torch.no_grad():  # No need to track gradients for this operation
         for inputs, _ in trainloader:
+
+            inputs = inputs.to(device)
             outputs = model(inputs)
+
             soft_labels.extend(softmax(outputs).tolist())
             data_list.extend(inputs)
 
@@ -62,13 +72,22 @@ def get_soft_labels(model, trainloader):
     return soft_labels_loader
 
 
-def generate_noise_dataloader(size, batch_size=4):
+def generate_noise_dataloader(size, device, batch_size=4):
 
     data = torch.randn(size, 1, 28, 28)
     data = (data - 0.5) / 0.5
     labels = torch.randint(0, 10, (size,))
 
+    data, labels = data.to(device), labels.to(device)
+
     dataset = TensorDataset(data, labels)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     return dataloader
+
+def visualize_img(tensor):
+
+    image_tenor = tensor * 0.5 + 0.5
+    numpy_image = image_tenor.numpy()
+    plt.imshow(numpy_image[0], cmap='gray')
+    plt.show()
