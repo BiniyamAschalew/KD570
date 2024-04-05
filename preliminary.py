fixed issue with get_predicted_labels functions in utils.py :- the output labels were set as softmax probabilities, however since nn.crossentropy takes only hard labels, for the time being converted the model to return the hard labels by the trainer model

preliminary.py
@@ -7,7 +7,8 @@ import numpy as np
 
 # setting a fixed seed
 seed_value = 19
-BATCH_SIZE = 4
+BATCH_SIZE = 32
+
 
 torch.manual_seed(seed_value)
 if torch.cuda.is_available():
@@ -45,12 +46,12 @@ mnist_soft_labels_loader = get_soft_labels(teacher, trainloader, device)
 print("finished getting soft labels for MNIST")
 
 # mnist dataset has 60k images, for the noise we will generate 600k images
-noise_loader = generate_noise_dataloader(600000, device, BATCH_SIZE)
+noise_loader = generate_noise_dataloader(100000, device, BATCH_SIZE)
 noise_soft_labels_loader = get_soft_labels(teacher, noise_loader, device)
 print("finished getting soft labels for noise")
 
 student0 = train_network(student0, noise_soft_labels_loader, 1, 0.001, device)
-student1 = train_network(student1, trainloader, 1, 0.001, device)
+# student1 = train_network(student1, trainloader, 1, 0.001, device)
 student2 = train_network(student2, mnist_soft_labels_loader, 1, 0.001, device)
 
 print("evaluating the four students")
@@ -58,7 +59,7 @@ print("evaluating student 0 which is trained on soft labels from noise")
 test_network(student0, testloader, device)
 
 print("evaluating student 1 which is trained on hard labels")
-test_network(student1, testloader, device)
+# test_network(student1, testloader, device)
 
 print("evaluating student 2 which is trained on soft labels")
 test_network(student2, testloader, device)

utils.py
@@ -50,7 +50,7 @@ def test_network(model, testloader, device):
     return correct / total
 
 
-def get_soft_labels(model, trainloader, device):
+def __get_soft_labels(model, trainloader, device):
     model.eval()  # Set the model to evaluation mode
     soft_labels = []
     data_list = []
@@ -70,6 +70,27 @@ def get_soft_labels(model, trainloader, device):
     soft_labels_loader = DataLoader(soft_labels_dataset, batch_size=trainloader.batch_size)
 
     return soft_labels_loader
+
+
+def get_predicted_labels(model, trainloader, device):
+    model.eval() 
+    predicted_labels = []
+    data_list = []
+
+    with torch.no_grad():  
+        for inputs, _ in trainloader:
+
+            inputs = inputs.to(device)
+            outputs = model(inputs)
+
+            _, preds = torch.max(outputs, 1)  
+            predicted_labels.extend(preds.cpu().tolist())
+            data_list.extend(inputs)
+
+    predicted_labels_dataset = TensorDataset(torch.stack(data_list), torch.tensor(predicted_labels))
+    predicted_labels_loader = DataLoader(predicted_labels_dataset, batch_size=trainloader.batch_size)
+
+    return predicted_labels_loader
 
 
 def generate_noise_dataloader(size, device, batch_size=4):