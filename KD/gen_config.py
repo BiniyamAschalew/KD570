21a453333923d3275baaa4bb64f91e9e1d19cb87
Biniyam Aschalew <aschalbiniyam@gmail.com>
2024-05-09 15:16:07 +0900

    added dataset configs

--- KD/gen_config.py
+++ KD/gen_config.py
@@ -1,6 +1,8 @@
 from typing import List
 import os
 
+from utils import load_config
+
 def write_config(config_dir: str, content: str):
 
     with open(config_dir, "w") as file:
@@ -12,8 +14,6 @@ def create_model_config(
     pretrained: bool = True,
     load_from_path: bool = False,
     model_path: str = "./models",
-    input_shape: List[int] = [1, 28, 28],
-    num_classes: int = 10,
 
     size: List[int] = [16, 32],
     kernel_size: List[int] = [3, 3],
@@ -21,11 +21,22 @@ def create_model_config(
     padding: List[int] = [0, 0],
     dropout: List[float] = [0, 0],
 
+    save_model: bool = True,
     save_path: str = "./models/sample_model.pth",
+
+    dataset: str = "MNIST",
+    dataset_config_dir: str = "configs/dataset_configs",
+
     config_file_name: str = "sample_model_config.yaml",
     config_dir: str = "configs/model_configs"
 
     ) -> str:
+
+    dataset_config_dir = os.path.join(dataset_config_dir, f"{dataset}.yaml")
+    dataset_config = load_config(dataset_config_dir)
+
+    input_shape = dataset_config["input_shape"]
+    num_classes = dataset_config["num_classes"]
 
     assert(len(size) == len(kernel_size) == len(stride) == len(padding) == len(dropout))
 
@@ -42,7 +53,11 @@ kernel_size: {kernel_size}
 stride: {stride}
 padding: {padding}
 dropout: {dropout}
-save_path: {save_path}"""
+
+dataset: {dataset}
+
+save_path: {save_path}
+save_model: {save_model}"""
 
     config_dir = os.path.join(config_dir, config_file_name)
     write_config(config_dir, model_config)
@@ -56,9 +71,9 @@ def create_main_config(
     epochs: int = 2,
     learning_rate: float = 0.001,
     device: str = "cuda",
-    multiprocess: bool = True,
+    multiprocess: bool = False,
 
-    temperature: float = 1,
+    temperature: float = 20,
     distillation_weight: float = 0.5,
     ce_weight: float = 0.5,
 
@@ -99,10 +114,28 @@ log_dir: {log_dir}"""
     write_config(config_dir, main_config)
     return main_config
 
+def create_dataset_config(
+    dataset: str = "MNIST",
+    input_shape: List[int] = [1, 28, 28],
+    num_classes: int = 10,
+    config_dir: str = "configs/dataset_configs"
+    ) -> str:
+
+    config_file_name = f"{dataset}.yaml"
+
+    dataset_config = f"""dataset: {dataset}
+input_shape: {input_shape}
+num_classes: {num_classes}"""
+
+    config_dir = os.path.join(config_dir, config_file_name)
+    write_config(config_dir, dataset_config)
+    return dataset_config
+
 def main():
     
     create_model_config(config_file_name = "example_model_config.yaml")
     create_main_config(config_file_name = "example_config.yaml")
+    # create_dataset_config()
 
 if __name__ == "__main__":
     main()
