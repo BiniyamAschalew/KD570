1b410a9f366266618777603af08bea6366110160
Biniyam Aschalew <aschalbiniyam@gmail.com>
2024-05-11 14:19:37 +0900

    update set_seed function to account for cudnn undeterministic behavior

--- KD/utils.py
+++ KD/utils.py
@@ -48,10 +48,13 @@ def set_seed(seed):
 
     random.seed(seed)
     numpy.random.seed(seed)
-
     torch.manual_seed(seed)
+
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
+        
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
     
 
